b9a6a14de53ae6ace9b449f35cc4c8df6abbf446
Juri Sajzew <j.sajzew@outlook.de>
2024-02-23 12:27:33 +0100

    add fonts and picture generate header component

--- src/app/src/app/landingpage/header/header.component.ts
+++ src/app/src/app/landingpage/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  colorClass: string = 'button-header';
 
+  changeStyle(event: MouseEvent) {
+    this.colorClass = event.type === 'mouseover' ? 'button-header:hover' : 'button-header';
+  }
 }
